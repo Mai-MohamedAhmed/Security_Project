56112325f6c8f9939cddad372d1b6d12fb51f822
Mai <mai.mawadalla@gmail.com>
2018-03-26 20:16:06 +0200

    added Ks enc & dec to main

--- src/main.java
+++ src/main.java
@@ -4,13 +4,20 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 
+import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.Key;
+
 import javax.crypto.SecretKey;
+
 import java.security.NoSuchAlgorithmException;
+
 import javax.xml.bind.DatatypeConverter;
+
 import java.security.spec.*;
+
 import javax.crypto.spec.*;
+
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -28,6 +35,14 @@ public class main {
 		cipher.init(Cipher.DECRYPT_MODE, k);
         byte[] original = cipher.doFinal(enc_email);
         return original;
+	}
+	public static String btos(byte [] ba){
+		String s = "";
+		for(byte b: ba)
+		{
+			s+=Byte.toString(b);
+		}
+		return s;
 	}
 	public static void main(String[] args) throws IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException{
 		// TODO Auto-generated method stub
@@ -50,6 +65,12 @@ public class main {
 		KeyGenerator kg = KeyGenerator.getInstance("DES");
 		kg.init(56);
 		SecretKey  k = kg.generateKey();
+		
+		//Make sure the key has a positive value for RSA encryption
+		while(new BigInteger(k.getEncoded()).compareTo(BigInteger.ZERO)<0){
+			k = kg.generateKey();
+		}
+		
 		System.out.println("Secret Key value     :: " + DatatypeConverter.printBase64Binary(k.getEncoded()));
 		///////////////////////////////////////////Email Encryption//////////////////////////////////////////
 		Cipher cipher = Cipher.getInstance("DES");
@@ -57,17 +78,21 @@ public class main {
 		System.out.println("Encrypted email    :: " +new String(encrypted_msg));
 		
 		///////////////////////////////////////////key encryption////////////////////////////////////////
-		SecretKey encryptedKey=k;
+		RSA rsa = new RSA(64);
+		byte[] keyBytes = k.getEncoded();
+		byte[] encryptedKey = rsa.encrypt(keyBytes);
+		
 		//encryptedKey==RSA_enc(k,publicKey);
 		/////////////////////////////////////////////Send email/////////////////////////////////////
 		 String sent_msg;
-		 sent_msg=encryptedKey.toString()+new String(encrypted_msg);
+		 //sent_msg=encryptedKey.toString()+new String(encrypted_msg);
+		 sent_msg=btos(encryptedKey)+new String(encrypted_msg);
 		 System.out.println("Sent msg :: "+sent_msg);
 		 ////////////////////////////////////////////Receive email/////////////////////////////////
 		 
 		 ////////////////////////////////////////////Key Decryption/////////////////////////////////////////
-		 SecretKey ecryptedKey=k;
-		//encryptedKey==RSA_enc(k,publicKey);
+		 byte[] decryptedKey = rsa.decrypt(encryptedKey);
+		 Key ks = new SecretKeySpec(decryptedKey,0, decryptedKey.length,"DES");
 		///////////////////////////////////////////Email Decryption///////////////////////////////////////////
 		decrypted_msg=dec(cipher,encrypted_msg,k);
 		System.out.println("Decrypted email    :: " +new String(decrypted_msg));
