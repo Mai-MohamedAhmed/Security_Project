24588ac0b3229562b24acc90992943fb76d724f1
MaryamAbdelmoneam <maryam.m.abdelmoneam@gmail.com>
2018-05-09 22:56:19 +0200

    exceptions

--- src/RSA.java
+++ src/RSA.java
@@ -16,20 +16,23 @@ import java.util.Random;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-
+//Public Key
 class PuPair{
 	BigInteger e, n; 
 	public PuPair(BigInteger e1, BigInteger n1){
 		e=e1;n=n1;
 	}
 }
+//Private Key
 class PrPair{
 	BigInteger d, n;
 	public PrPair(BigInteger d1, BigInteger n1){
 		d=d1;n=n1;
 	}
 }
+//RSA Implementation
 public class RSA {
+	//Convert Byte array to string
 	public static String btos(byte [] ba){
 		String s = "";
 		System.out.println(ba.length);
@@ -39,19 +42,18 @@ public class RSA {
 		}
 		return s;
 	}
-
+	//Generate Random Object
 	private static SecureRandom random = new SecureRandom();
-	//private BigInteger publicKey;
-	//private BigInteger privateKey;
-	//private BigInteger n;
+	//No. of bits of n 
 	private int bits;
-	
+	//Constructor
 	public RSA(int b){
 		bits = b;
 		System.out.println("RSA "+bits+" bits");
 	}
 	public RSA(){
 	}
+	//Generate new Private, Public Pair
 	public void generate() throws IOException{
 		BigInteger n;
 		BigInteger d;
@@ -73,6 +75,7 @@ public class RSA {
 		System.out.println("e: "+e);
 		d = e.modInverse(phi);
 		System.out.println("d: "+d);
+	    //Write Public Key to File
 	    BufferedWriter pu = new BufferedWriter(new FileWriter("Public.txt", false));
 	    pu.write("e:");
 	    pu.newLine();
@@ -82,7 +85,7 @@ public class RSA {
 	    pu.newLine();
 	    pu.append(n.toString());
 	    pu.close();
-	    
+	    //Write Private Key to File
 	    BufferedWriter pr = new BufferedWriter(new FileWriter("Private.txt", false));
 	    pr.write("d:");
 	    pr.newLine();
@@ -91,27 +94,28 @@ public class RSA {
 	    pr.append("n:");
 	    pr.newLine();
 	    pr.append(n.toString());
-	    
 	    pr.close();
 		
 	}
+	//Ecrypt Big Integer
 	public BigInteger encrypt(BigInteger k, BigInteger e,BigInteger N){
 		return k.modPow(e, N);	
 	}
-	
+	//Encrypt Byte Array
 	public byte[] encrypt(byte[] k,BigInteger e,BigInteger N){
 		return (new BigInteger(k)).modPow(e, N).toByteArray();	
 	}
-	
+	//Decrypt Big Integer
 	public BigInteger decrypt(BigInteger k, BigInteger d, BigInteger N){
 		return k.modPow(d, N);	
 	}
-	
+	//Decrypt Byte Array
 	public byte[] decrypt(byte[] k, BigInteger d, BigInteger N){
 		return (new BigInteger(k)).modPow(d, N).toByteArray();	
 	}
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		//Different no. of key bits
 		//64 - 128 - 256 - 512 - [[1024]] - 2048 - 3072 - 15360**
 		//KS bits < N bits
 		RSA rsa = new RSA(1024);
@@ -125,27 +129,28 @@ public class RSA {
 			sk = kg.generateKey();
 		}
 		
-		 
+		 //Read Public Key
 		BufferedReader pu = new BufferedReader(new FileReader(new File("Public.txt")));
 		String st;
 		st = pu.readLine();
 		BigInteger e = new BigInteger(pu.readLine());
 		st = pu.readLine();
 		BigInteger n = new BigInteger(pu.readLine());
-	
+		
 		byte[] msg = sk.getEncoded();
 		
 		long startTime = System.nanoTime();
+		//Encrypt
 		byte[] enc = rsa.encrypt(msg,e,n);
 		long endTime = System.nanoTime();
-		
+		//Read Private Key
 		BufferedReader pr = new BufferedReader(new FileReader("Private.txt"));
 		
 		st = pr.readLine();
 		BigInteger d = new BigInteger(pr.readLine());
 		st = pr.readLine();
 		BigInteger n2 = new BigInteger(pr.readLine());
-		
+		//Decrypt
 		byte[] dec = rsa.decrypt(enc,d,n2);
 		
 		Key k = new SecretKeySpec(dec,0, dec.length,"DES");
@@ -155,29 +160,7 @@ public class RSA {
 		System.out.println("Decrypted key bytes: "+ RSA.btos(dec));
 		long duration = (endTime - startTime);
 		System.out.println("Time : "+duration);
-	
-		
-		
-		//string from key
-				//SecretKey sk = KeyGenerator.getInstance("DES").generateKey();
-				//String s = Base64.getEncoder().encodeToString(sk.getEncoded());
-			
-		//KeyGenerator kg=KeyGenerator.getInstance("DES");
-		//Key key=kg.generateKey();
-		//String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
-		//System.out.println(key);
-		//System.out.println(encodedKey);
-		/*
-		System.out.println("M: "+m);
-		long startTime = System.nanoTime();
-		BigInteger c = rsa.encrypt(m);
-		long endTime = System.nanoTime();
-		System.out.println("c: "+c);
-		BigInteger d = rsa.decrypt(c);
-		System.out.println("d: "+d);
-		long duration = (endTime - startTime);
-		System.out.println("Time : "+duration);
-		*/
+
 	}
 
 }
